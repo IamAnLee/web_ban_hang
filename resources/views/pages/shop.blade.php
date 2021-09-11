c62b8777293f1bc9eccec34c65d8ca646bcd4a6b
unknown <anle101201@gmail.com>
2021-09-11 20:20:28 +0700

    update

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -2,7 +2,7 @@
 <html lang="zxx">
 
 <head>
-	<title>Downy Shoes an Ecommerce Category Bootstrap Responsive Website Template | Shop :: w3layouts</title>
+	<title> Shop </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="keywords" content="Downy Shoes Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
